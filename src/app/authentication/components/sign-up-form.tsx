c2201cd8a1f1cb278b25132b611d583eddfa24f4
HenriqueCoruqieri <heoliveirac@gmail.com>
2025-08-06 22:06:30 -0300

    feat: add sign in with email and passoword

--- src/app/authentication/components/sign-up-form.tsx
+++ src/app/authentication/components/sign-up-form.tsx
@@ -53,10 +53,11 @@ const SignUpForm = () => {
                 onError: (error) => {
                     if(error.error.code == "USER_ALREADY_EXISTS") {
                         toast.error("Email já cadastrado"); 
-                        form.setError("email", {
+                        return form.setError("email", {
                             message: "Email já cadastrado",
                         });
                     }
+                    toast.error(error.error.message);
                 },
             },
         });
